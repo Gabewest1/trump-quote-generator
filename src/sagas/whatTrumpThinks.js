27f49127c175a4973df1eecb51e766e4b5393755
Gabriel West <gabe_west1@yahoo.com>
2017-07-12 18:09:58 -0500

    Added while loop and a take effect

--- src/sagas/whatTrumpThinks.js
+++ src/sagas/whatTrumpThinks.js
@@ -1,12 +1,15 @@
 import { delay } from "redux-saga"
-import { call, put } from "redux-saga/effects"
+import { call, put, take } from "redux-saga/effects"
 
 const URL = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
 
 export function* getRandomQuote() {
-    let response = yield call(fetch, URL)
-    let quote = yield response.json()
-    quote = quote.message
+    while(true) {
+        yield take("GET_RANDOM_QUOTE")
+        let response = yield call(fetch, URL)
+        let quote = yield response.json()
+        quote = quote.message
 
-    yield put({type: "SET_CURRENT_QUOTE", quote: {text: quote}})
+        yield put({type: "SET_CURRENT_QUOTE", quote: {text: quote}})
+    }
 }
