2c57fed9a3014c9c67342ad9b5cfeb354f4f801c
Gabriel West <gabe_west1@yahoo.com>
2017-07-12 16:30:24 -0500

    Made directories for the reducers and sagas files

--- src/reducers.js
+++
@@ -1,7 +0,0 @@
-//Import your reducers and add them to the rootReducer
-
-const rootReducer = {
-    
-}
-
-export default rootReducer
--- src/sagas.js
+++
@@ -1,7 +0,0 @@
-//Import your sagas and add them to the rootSaga function
-
-export default function* rootSaga() {
-    yield [
-
-    ]
-}
