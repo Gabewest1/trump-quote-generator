b001bb397e86455a63b221fda8221b6fa0d60b81
Gabriel West <gabe_west1@yahoo.com>
2017-07-12 23:11:37 -0500

    Created saga to get a random quote

---
+++ src/sagas/getRandomQuote.js
@@ -0,0 +1,19 @@
+import { call, put, take } from "redux-saga/effects"
+import { constants as quotesConstants, actions as quoteActions } from "../actions/quotes"
+
+import { getAdministrationQuote } from "./getAdministrationQuote"
+import { getTrumpQuote } from "./getTrumpQuote"
+
+export function* getRandomQuote() {
+    while(true) {
+        yield take(quotesConstants.GET_RANDOM_QUOTE)
+        
+        let coinFlip = Math.floor(Math.random() * 2) % 2 === 0 ? "heads" : "tails"
+        console.log("coinFlip:", coinFlip)
+        let quote = coinFlip === "heads" 
+                            ? yield getAdministrationQuote()
+                            : yield getTrumpQuote()
+
+        yield put(quoteActions.setCurrentQuote(quote))
+    }
+}
