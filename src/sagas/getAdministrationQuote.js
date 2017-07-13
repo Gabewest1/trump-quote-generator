b5340ead3d5b4577d110b01d557c4355633ebeec
Gabriel West <gabe_west1@yahoo.com>
2017-07-12 23:15:21 -0500

    removed log

--- src/sagas/getAdministrationQuote.js
+++ src/sagas/getAdministrationQuote.js
@@ -1,14 +1,13 @@
 import { call, put, take } from "redux-saga/effects"
 import { constants as quotesConstants } from "../actions/quotes"
 
-const URL = "admin.json"
+const URL = "administrationQuotes.json"
 
 export function* getAdministrationQuote() {
         let response = yield call(fetch, URL)
         let { quotes } = yield response.json()
 
         let random = Math.floor(Math.random() * quotes.length)
-        console.log(quotes.length)
 
         return quotes[random]
 }
