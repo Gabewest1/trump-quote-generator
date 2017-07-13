cba4773bf95960932ce41ec03900a6a616f6d984
Gabriel West <gabe_west1@yahoo.com>
2017-07-13 18:07:54 -0500

    Removed unneeded Container. Removed unused blue and white color imports. Added component to float the author and position to the right. Set images max-height to 2/3 window height

--- src/components/Quote/styles.js
+++ src/components/Quote/styles.js
@@ -1,16 +1,14 @@
 import styled from "styled-components"
 import { Textfit } from "react-textfit"
-import { red, blue, white } from "../../theme/colors"
+import { red } from "../../theme/colors"
 
 export let ImageQuoteContainer = styled.div`
-    background: ${red};
+    background: black;
     position: relative;
     width: 100%;
     display: flex;
     align-items: center;
-    // padding: 30px;
 `
-
 export let QuoteContainer = styled.div`
     display: flex;
     width: 50%;
@@ -18,23 +16,21 @@ export let QuoteContainer = styled.div`
     right: 0;
     justify-content: center;
 `
-
-export let Container = styled.div`
-    background-color: ${red};
-    display: flex;
-    justify-content: center;
+export let Signature = styled.span`
+    float: right;
 `
-
 export let Image = styled.img`
     max-width: 100%;
-    // height: 500px;
+    max-height: ${() => window.innerHeight/1.5}px;
 `
 export let Quote = styled.h1`
     color: white;
-    font-size: inherit;
+    font-size: 24px;
     width: 90%;
     margin: 0 auto;
     font-style: italic;
+    line-height: 1.5;
+    white-space: pre-line;
 
     @media (max-width: 768px) {
         font-size: 16px;
@@ -43,6 +39,5 @@ export let Quote = styled.h1`
     @media (max-width: 440px) {
         font-size: 12px;
         line-height: 1.3;
-        // width: 40%;
     }
 `
