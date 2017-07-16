3bba7be5e770817bf10c3abd74dc0b5a4d2c9b77
Gabriel West <gabe_west1@yahoo.com>
2017-07-15 20:00:57 -0500

    Styled the fetch new quote button

--- src/components/Button/FetchQuote/styles.js
+++ src/components/Button/FetchQuote/styles.js
@@ -1,5 +1,8 @@
 import styled from "styled-components"
 
 export const Button = styled.button`
-
+    color: white;
+    padding: 20px 30px;
+    background: #151414;
+    border: solid 4px gray;
 `
