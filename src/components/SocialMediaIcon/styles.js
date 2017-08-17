8681d36792be9e93679fb0a271cb7fc7586eb1dd
Gabewest1 <gabe_west1@yahoo.com>
2017-08-16 20:34:28 -0500

    Made SocialMediaButton background blue.

--- src/components/SocialMediaIcon/styles.js
+++ src/components/SocialMediaIcon/styles.js
@@ -1,11 +1,14 @@
 import styled from "styled-components"
+import { blue } from "../../theme/colors"
 
 export const Icon = styled.img`
     float: right;
     width: 50px;
+    color: ${blue};
+    background: ${blue};
 `
 export const Button = styled.button`
-    background: #929292;
+    background: ${blue};
     display: flex;
     align-items: center;
     color: white;
