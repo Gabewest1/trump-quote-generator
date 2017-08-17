323d6ee9b2cdb585bfea7abfe037afac8c840c48
Gabewest1 <gabe_west1@yahoo.com>
2017-08-16 20:31:55 -0500

    Removed border and made background blue.

--- src/components/Button/FetchQuote/styles.js
+++ src/components/Button/FetchQuote/styles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components"
+import { blue } from "../../../theme/colors"
 
 export const Button = styled.button`
     color: white;
     padding: 20px 30px;
-    background: #151414;
-    border: solid 4px gray;
+    background: ${blue};
 `
