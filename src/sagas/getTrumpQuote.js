4cd7148aaebab6ed13fad33fa18c2437b0444eab
Gabewest1 <gabe_west1@yahoo.com>
2017-08-18 19:58:48 -0500

    Added logic to wait until the initial trump quotes are fetched, before trying to pick a random quote.

--- src/sagas/getTrumpQuote.js
+++ src/sagas/getTrumpQuote.js
@@ -1,9 +1,18 @@
-import { select } from "redux-saga/effects"
+import { select, take } from "redux-saga/effects"
 import { constants as quotesConstants } from "../actions/quotes"
 
 export function* getTrumpQuote() {
     let state = yield select()
     let trumpQuotes = state.trumpQuotes
+
+    //The first time this function gets called there isn't
+    //any trump quotes so need to wait for them to be fetched
+    if (trumpQuotes.length === 0) {
+        yield take("FETCH_TRUMP_QUOTES_SUCCESS")
+        state = yield select()
+        trumpQuotes = state.trumpQuotes
+    }
+
     let randomIndex = Math.floor(Math.random() * trumpQuotes.length)
 
     let message = trumpQuotes[randomIndex]
