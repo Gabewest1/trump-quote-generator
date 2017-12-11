fc409ec6c72472c26fb059fc1326a36c243740a5
Gabriel West <gabe_west1@yahoo.com>
2017-12-10 22:44:51 -0600

    Updated <FetchQuote/> button to not import acions and connect to redux.

--- src/components/Button/FetchQuote/index.js
+++ src/components/Button/FetchQuote/index.js
@@ -1,32 +1,15 @@
 import React from "react"
 import { connect } from "react-redux"
-import { bindActionCreators } from "redux"
-
 import { Button } from "./styles"
-import { actions as quoteActions } from "../../../actions/quotes"
 
 class FetchQuoteButton extends React.Component {
-    onClick = () => {
-        this.props.onClick()
-        this.props.fetchRandomQuote()
-    }
     render() {
-        let { children, fetchRandomQuote } = this.props
-
         return (
-            <Button { ...this.props } onClick={ this.onClick }>
-                { children }
+            <Button { ...this.props }>
+                { this.props.children }
             </Button>
         )
     }  
 }
 
-function mapStateToProps(state) {
-    return {}
-}
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({...quoteActions}, dispatch)
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(FetchQuoteButton)
+export default FetchQuoteButton
