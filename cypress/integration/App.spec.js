eccb8b37e25a3915765196e5e3d69b2b5dc23ab6
Gabriel West <gabe_west1@yahoo.com>
2018-04-12 22:44:31 -0500

    Added some empty tests to check if the page renders a title, quote and two buttons.

---
+++ cypress/integration/App.spec.js
@@ -0,0 +1,17 @@
+describe("App", () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+    })
+    it("should display a title", () => {
+
+    })
+    it("should display a quote", () => {
+
+    })
+    it("should display a button to get a new quote", () => {
+
+    })
+    it("should display a button to share the quote on twitter", () => {
+
+    })
+})
